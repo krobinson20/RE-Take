7890b87c8d64adaf7d17cb237d45ef58a941343a
Keenan Robinson <krobinson20@my.whitworth.edu>
2020-05-14 22:57:01 -0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'dart:async';
 import 'dart:io';
-
+import 'dart:ui';
 import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+
 
 int _selectedIndex = 0; //global variable for what icon is selected on the bottom bar
 File StoredImage;
 var selimage = 0;
-double transval = 0.4;
+double transval = 0.4; //value for the transparency
 const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-const List<Widget> _widgetOptions = <Widget>[
 
+//Info for the bottom navigation Bar
+const List<Widget> _widgetOptions = <Widget>[
   Text(
     'Index 1: Take Picture',
     style: optionStyle,
@@ -38,7 +42,10 @@ Future<void> main() async {
 
   runApp(
     MaterialApp(
-      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.cyan,
+        brightness: Brightness.light,),
       home: TakePictureScreen(
         // Pass the appropriate camera to the TakePictureScreen widget.
         camera: firstCamera,
@@ -86,18 +93,21 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     _controller.dispose();
     super.dispose();
   }
-pickImageFromGallery(ImageSource source) async {
+  //Function that allows the user to select an image from their gallery
+  pickImageFromGallery(ImageSource source) async {
 
-  File tempimg = await ImagePicker.pickImage(source: source);
-  selimage = 1;
+    //holder variable for the selected image
+    File tempimg = await ImagePicker.pickImage(source: source);
+    //boolean for if the user has picked an image or not
+    selimage = 1;
 
-
+ // set the state of StoredImage to the temporary variable//this is because the StoredImage variable cant just be passed the selected image
     setState(() {
       // imageFile = ImagePicker.pickImage(source: source);
-       StoredImage = tempimg;
+      StoredImage = tempimg;
     });
 
-}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -107,119 +117,228 @@ pickImageFromGallery(ImageSource source) async {
       // camera preview. Use a FutureBuilder to display a loading spinner
       // until the controller has finished initializing.
 
-    bottomNavigationBar: BottomNavigationBar(
-    items: const<BottomNavigationBarItem>[
-    BottomNavigationBarItem(
-    icon: Icon(Icons.photo_library),
-    title: Text('Select a Picture'),
-    ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const<BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_library),
+            title: Text('Select a Picture'),
+          ),
 
-    BottomNavigationBarItem(
-    icon:  Icon(Icons.camera),
-    title : Text('Take a Picture'),
-    )
-    ],
+          BottomNavigationBarItem(
+            icon:  Icon(Icons.camera),
+            title : Text('Take a Picture'),
+          )
+        ],
 
-    currentIndex: _selectedIndex,
-      onTap: (currentIndex){
-      setState(() {
-        switch(currentIndex){
-          case 0:
-            pickImageFromGallery(ImageSource.gallery);
+        currentIndex: _selectedIndex,
+        onTap: (currentIndex){
+          setState(() {
+            switch(currentIndex){
+              case 0:
+                //case 0 if the user taps select and image
+                //function call for selecting the image from the gallery
+                pickImageFromGallery(ImageSource.gallery);
 
-            break;
-          case 1:
-            getTemporaryDirectory().then((d){
-              var path = join(d.path, '${DateTime.now()}.png');
-              _controller.takePicture(path).then((v){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DisplayPictureScreen(imagePath: path),
-                  ),
-                );
-              });
-            });
-            break;
-
-    }
-      });
-    },
-    ),
+                break;
+              case 1:
+                //case 1 for if the user taps take a picture
+                getTemporaryDirectory().then((d){
+                  var path = join(d.path, '${DateTime.now()}.png');
+                  _controller.takePicture(path).then((v){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DisplayPictureScreen(imagePath: path),
+                      ),
+                    );
+                  });
+                });
+                break;
+            }
+          });
+        },
+      ),
 
       body: FutureBuilder<void>(
         future: _initializeControllerFuture,
 
         builder: (context, snapshot) {
-    if (selimage != 0) {
-      if (snapshot.connectionState == ConnectionState.done) {
-        // If the Future is complete, display the preview.
-        return Stack(
-          children: <Widget>[
-            CameraPreview(_controller),
-            Container(
-              decoration: BoxDecoration(
-                image: new DecorationImage(
-                    colorFilter: new ColorFilter.mode(Colors.black.withOpacity(transval), BlendMode.dstATop),
-                    //OPACITY
-                    image: new FileImage(StoredImage),
-                    fit: BoxFit.fitHeight),
-              ),
-            ),
-           Slider.adaptive(value: transval,
-                onChanged: (newValue){
-                  setState(() => transval = newValue);
-                },
-            ),
-          ],
-        );
-        //return CameraPreview(_controller);
-      } else {
-        // Otherwise, display a loading indicator.
-        return Center(child: CircularProgressIndicator());
-      }
-    }else{
-      return  Center(child: Text("Plese Select an Image"));
-    }
-          },
+          //if there is a selected image then show the transparent preview of the image
+          if (selimage != 0) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If the Future is complete, display the preview.
+              return Stack(
+                children: <Widget>[
+                  CameraPreview(_controller),
+                  Container(
+                    decoration: BoxDecoration(
+                      image: new DecorationImage(
+                        //overlaid image with variable transparency value
+                          colorFilter: new ColorFilter.mode(Colors.black.withOpacity(transval), BlendMode.dstATop),
+                          image: new FileImage(StoredImage),
+                          fit: BoxFit.fitHeight),
+                    ),
+                  ),
+
+                  //slider for changing the transparency value
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    child: Slider.adaptive(value: transval,
+                      onChanged: (newValue){
+                        setState(() => transval = newValue);
+                      },
+                    ),
+                  ),
+                ],
+
+              );
+              //return CameraPreview(_controller);
+            } else {
+              // Otherwise, display a loading indicator.
+              return Center(child: CircularProgressIndicator());
+            }
+          }
+          //if the user has not selected an image then tell them to select one
+          else{
+            return  Center(child: Text("Plese Select an Image"));
+          }
+        },
 
       ),
     );
   }
-
-
 }
 
 // A widget that displays the picture taken by the user.
 class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
-
+  final String imagePath; // variable for the path of the image that was just taken
   const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
 
 
-   // static var ms = (new DateTime.now()).millisecondsSinceEpoch;
-  // static double opac = (ms / 1000) % 10;
-
   @override
-
   Widget build(BuildContext context) => new Scaffold(
 
     appBar: new AppBar(
       title: new Text('RE-Take'),
     ),
+
     body: Stack(
       children: <Widget>[
         Container(
           decoration: BoxDecoration(
             image: new DecorationImage(
                 colorFilter: new ColorFilter.mode(Colors.black.withOpacity(1), BlendMode.dstATop), //OPACITY
-                image: new FileImage(File(imagePath)),
+                image: new FileImage(File(imagePath)), //the image that was taken
                 fit: BoxFit.fitHeight),
 
           ),
+
         ),
-        
+        Container(
+          child: Align(
+            alignment: FractionalOffset.bottomCenter,
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                // button for comparing the before and after images
+                new RaisedButton.icon(
+                  icon: Icon(Icons.autorenew),
+                  color: Colors.cyanAccent[100],
+                  textColor: Colors.black,
+                  label: Text('Compare'),
+                  elevation: 55,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Compare(imagePath: imagePath),
+                      ),
+                    );
+                  },
+                ),
+
+                // button for saving the newly taken picture
+                new RaisedButton.icon(
+                  icon: Icon(Icons.save),
+                  color: Colors.cyanAccent[100],
+                  textColor: Colors.black,
+                  label: Text('Save'),
+                  elevation: 55,
+                  onPressed: () {
+                    GallerySaver.saveImage(imagePath).then((bool success) {
+                      print(success);
+                    });},
+                ),
+              ],
+            )
+          )
+        )
       ],
     ),
+
+  );
+
+}
+
+// if the user taps compare it shows both the before and after images
+class Compare extends StatelessWidget{
+  final String imagePath;
+  const Compare({Key key, this.imagePath}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+
+      appBar: new AppBar(
+        title: new Text('RE-Take'),
+      ),
+
+
+      body: Stack(
+        children: [
+          new Column(children:[
+            Container(
+              alignment: Alignment.topCenter,
+              height: (MediaQuery.of(context).size.height * 0.5)-50,
+              width: MediaQuery.of(context).size.width,
+              //color: Colors.green,
+              decoration: BoxDecoration(
+                image: new DecorationImage(
+                    colorFilter: new ColorFilter.mode(Colors.black.withOpacity(1), BlendMode.dstATop), //OPACITY
+                    image: new FileImage(File(imagePath)),
+                    fit: BoxFit.fitHeight),
+
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              height: (MediaQuery.of(context).size.height * 0.5)-50,
+              width: MediaQuery.of(context).size.width,
+              //color: Colors.blue,
+              decoration: BoxDecoration(
+                image: new DecorationImage(
+                    colorFilter: new ColorFilter.mode(Colors.black.withOpacity(1), BlendMode.dstATop), //OPACITY
+                    image: new FileImage(StoredImage),
+                    fit: BoxFit.fitHeight),
+
+              ),
+            )
+          ],
+          ),
+          //button for saving the image
+          RaisedButton.icon(
+            icon: Icon(Icons.save),
+            color: Colors.cyanAccent[100],
+            textColor: Colors.black,
+            label: Text('Save'),
+            elevation: 55,
+            onPressed: () {
+              GallerySaver.saveImage(imagePath).then((bool success) {
+                print(success);
+              });},
+          ),
+
+        ],
+      )
   );
 }
